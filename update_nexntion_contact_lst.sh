88055c805634188c3e853d11c6ec0eb487c23bd1
ytsqkl <58875910+ytsqkl@users.noreply.github.com>
2025-05-16 22:55:12 +0800

    Update update_nexntion_contact_lst.sh

--- update_nexntion_contact_lst.sh
+++ update_nexntion_contact_lst.sh
@@ -1,6 +1,15 @@
-lst
+mount -o remount,rw /
+echo "Download DMRIds and Hostfiles.." 
 rpi-rw  
 cd /tmp  
 sudo git clone https://gitee.com/ytqkl/mmdvm-CNcontact.git  
 cd /tmp/mmdvm-CNcontact   
-sudo cp stripped.csv /usr/local/etc
+sudo mv stripped.csv /usr/local/etc
+mount -o remount,ro /
+sudo systemctl stop pistar-watchdog.service > /dev/null 2>&1
+sudo systemctl stop mmdvmhost.service > /dev/null 2>&1 && sleep 2 > /dev/null 2>&1 
+sudo systemctl start pistar-watchdog.service# > /dev/null 2>&1
+sudo systemctl start mmdvmhost.service #> /dev/null 2>&1 && sleep 2 > /dev/null 2>&1
+ls -l /usr/local/etc
+echo "Done. save to /usr/local/etc";
+echo "Done";
